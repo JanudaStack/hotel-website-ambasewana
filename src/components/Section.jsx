2ea35df0b1c53cccacbe44c5b8b78f369c97e267
JanudaStack <jaariyasinghe2003@gmail.com>
2025-02-27 23:58:59 +0530

    Update hero section and styles; add responsive design adjustments

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,34 @@
 function Hero() {
-    return;
+    return (
+        <>
+            <section className="hero">
+                <div className="hero-text">
+                    {/* <h5>✨ Unwind. Indulge. Discover. ✨</h5> */}
+                    <h1>
+                        Welcome to Hotel <br />
+                        Ambasewana
+                    </h1>
+                    <p>Where Heritage Meets Luxury!</p>
+                </div>
+            </section>
+            <div className="icons">
+                <a href="">
+                    <i className="bx bxl-facebook"></i>
+                </a>
+                <a href="">
+                    <i className="bx bxl-youtube"></i>
+                </a>
+                <a href="">
+                    <i className="bx bxl-twitter"></i>
+                </a>
+            </div>
+            <div className="scroll">
+                <a href="#">
+                    <i className="bx bx-down-arrow-alt"></i>
+                </a>
+            </div>
+        </>
+    );
 }
 
 export default Hero;
