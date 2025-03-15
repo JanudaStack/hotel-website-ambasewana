30fbf6a4b10a41e14ff0681998d53d9d4b73da66
JanudaStack <jaariyasinghe2003@gmail.com>
2025-03-15 20:11:26 +0530

    Added routing functionality to button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -104,7 +104,7 @@ function Header() {
                             <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
                         </svg>
                     </button>
-                    <div className="book-now">
+                    <div className="book-now" onClick={routeChange}>
                         <div className="btn-text">
                             <button>
                                 Book Now <i className="ri-arrow-right-line"></i>
