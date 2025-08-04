0e28dda1a4aae2a6c2878e7fc07f64caa26fcf10
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-08-04 23:41:39 +0530

    fix: remove unused code snippets from Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,6 @@
 const Card = () => {
     return (
         <>
-            {/* <div className="section-spacing-2"></div> */}
             <div className="card-container">
                 <div className="heading">
                     <h2>Our Amenities</h2>
@@ -10,42 +9,18 @@ const Card = () => {
                     <div className="card">
                         <i className="ri bx bxs-dish"></i>
                         <h3>Room service</h3>
-                        {/* <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Atque assumenda iste aut sunt quas fugit repellendus
-                        rerum ut corrupti nam neque provident error laudantium,
-                        accusamus voluptas labore blanditiis magni minus.
-                    </p> */}
                     </div>
                     <div className="card" id="card-black">
                         <i className="ri bx bx-wifi"></i>
                         <h3>Free WiFi</h3>
-                        {/* <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Atque assumenda iste aut sunt quas fugit repellendus
-                        rerum ut corrupti nam neque provident error laudantium,
-                        accusamus voluptas labore blanditiis magni minus.
-                    </p> */}
                     </div>
                     <div className="card">
                         <i className="ri ri-parking-box-line"></i>
                         <h3>Free parking</h3>
-                        {/* <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Atque assumenda iste aut sunt quas fugit repellendus
-                        rerum ut corrupti nam neque provident error laudantium,
-                        accusamus voluptas labore blanditiis magni minus.
-                    </p> */}
                     </div>
                     <div className="card" id="card-black">
                         <i className="ri ri-restaurant-line"></i>
                         <h3>Dining</h3>
-                        {/* <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Atque assumenda iste aut sunt quas fugit repellendus
-                        rerum ut corrupti nam neque provident error laudantium,
-                        accusamus voluptas labore blanditiis magni minus.
-                    </p> */}
                     </div>
                 </div>
             </div>
